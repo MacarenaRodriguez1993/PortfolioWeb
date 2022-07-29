34e761fb4cf664571cde28567381dfa3600139a3
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2022-07-29 00:36:50 -0300

    inicio actualizaciones

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ particlesJS(
           },
           "move": {
             "enable": true,
-            "speed": 6,
+            "speed": 3,
             "direction": "none",
             "random": false,
             "straight": false,
